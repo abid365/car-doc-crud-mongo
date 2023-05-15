c479bf38c80e55860605e858ab94b647e7d69dae
jawad Bin Azam <abidibnazam@gmail.com>
2023-05-15 20:48:05 +0600

    rm env

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// DB_USER=carDoctor,
+// DB_PASS=7Y5jqfSbknCLkArB
+
 console.log(process.env.DB_PASS);
 console.log(process.env.DB_USER);
 
