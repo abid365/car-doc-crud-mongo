379bee6492bd48308b364ccd0cc8e6e03e15c0e9
jawad Bin Azam <abidibnazam@gmail.com>
2023-05-17 02:44:56 +0600

    patch mechanism

--- index.js
+++ index.js
@@ -82,9 +82,19 @@ async function run() {
       res.send(result);
     });
 
-    // bookings : update
+    //? bookings : update
     app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updateBooking = req.body;
+      console.log(updateBooking);
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
